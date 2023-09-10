a1331d5e22655eb78631c96d3e4efe412ad3de50
yowayimono <3485000346@qq.com>
2023-09-10 10:34:11 +0800

    conf

--- conf/config.go
+++ conf/config.go
@@ -13,13 +13,13 @@ var AppJsonConfig = []byte(`
 {
   "app": {
     "port": "8322",
-    "upload_file_path": "e:\\golang\\www\\go-gin-chat\\tmp_images\\",
+    "upload_file_path": "E:\\",
     "cookie_key": "4238uihfieh49r3453kjdfg",
     "serve_type": "GoServe",
     "debug_mod": "true"
   },
   "mysql": {
-    "dsn": "admin:admin@tcp(127.0.0.1:3306)/go_gin_chat?charset=utf8mb4&parseTime=True&loc=Local"
+    "dsn": "root:0503@tcp(127.0.0.1:3306)/go_gin_chat?charset=utf8mb4&parseTime=True&loc=Local"
   }
 }
 `)
